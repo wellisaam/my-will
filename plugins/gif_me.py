78ce8b8f346994b37b09065bd723807ff77d282f
wellisaam <William.ellis@artsalliancemedia.com>
2014-10-09 16:06:04 +0100

    moar stuff

--- plugins/gif_me.py
+++ plugins/gif_me.py
@@ -1,5 +1,5 @@
 from will.plugin import WillPlugin
-from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+from will.decorators import respond_to
 
 from helpers.gif_scott_heron import GifMeUpScotty
 
